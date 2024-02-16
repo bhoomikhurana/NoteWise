274b560116a8ca74691489fced4c438fbc408254
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2024-02-17 00:48:03 +0530

    initialised frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API is running bro");
+});
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
